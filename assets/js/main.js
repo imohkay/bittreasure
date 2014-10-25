5d11146f2f4388d1a2716a44230ebeb72dbae892
Karen Menezes <karen.menezes@gmail.com>
2014-10-25 22:31:16 +0530

    added validate-success classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
- /* $(".js-toggle").on("click", function(){
+  $(".js-toggle").on("click", function(){
     $(this).next().slideToggle("fast");
   });
+
+  $(".js-toggle-fade").on("click", function(){
+    $(this).next().addClass("b-fade-in");
+  });
+
 });
